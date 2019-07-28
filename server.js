2c87d164aff779858e2adb201c044e8b755c8a35
JeremiahMutuc <jeremiahmutuc_p@yahoo.com>
2019-07-28 10:01:30 +0800

    Successfully connected to database using Mysql

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
 
 var app = express();
 var router = express.Router();
 
+//first route
 app.get('/',function(req, res){
     res.send('Hello first route');
 });
 
-app.listen(1300, function() {
-    console.log('Im listening');
+//Creating connection to mysql
+var sqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'blank',
+    database: 'inventory'
 });
+
+//Connecting to mysql
+sqlConnection.connect((err)=>{
+      if(!err){
+        console.log('Success!');
+      }
+      else{
+          console.log('Failed error: '+JSON.stringify(err,undefined,2));
+      }
+});
+
+//listen to port:1300
+app.listen(1300);
